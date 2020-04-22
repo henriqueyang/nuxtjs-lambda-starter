8d07f54ba544e0c6df4c2134edabdb2077a89f78
Henrique <henriqueyang17@gmail.com>
2020-04-22 18:10:56 -0300

    IIFE for start no-production mode

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-// const path = require('path')
 const { Nuxt, Builder } = require('nuxt')
 const fastify = require('fastify')({
   logger: true,
@@ -12,21 +11,23 @@ const nuxt = new Nuxt(config)
 fastify.use(nuxt.render)
 
 if (require.main === module) {
-  nuxt.ready()
-  // Build only in dev mode
-  const builder = new Builder(nuxt)
-  builder.build()
+  ;(async () => {
+    await nuxt.ready()
+    // Build only in dev mode
+    const builder = new Builder(nuxt)
+    builder.build()
 
-  fastify.listen(
-    process.env.PORT || 3000,
-    process.env.HOST || '127.0.0.1',
-    (err, address) => {
-      if (err) {
-        fastify.log.error(err)
-        process.exit(1)
+    fastify.listen(
+      process.env.PORT || 3000,
+      process.env.HOST || '127.0.0.1',
+      (err, address) => {
+        if (err) {
+          fastify.log.error(err)
+          process.exit(1)
+        }
       }
-    }
-  )
+    )
+  })()
 } else {
   exports.server = async () => {
     await nuxt.ready()
